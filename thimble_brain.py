5485b00c9c0a313a3eaf2813f783b1006d22a814
Veith Roethlingshoefer <veith.roethlingshoefer@gmail.com>
2018-02-06 23:06:07 +0100

    Implement minimal version that inits with green tracker neurons

--- thimble_brain.py
+++ thimble_brain.py
@@ -10,22 +10,6 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-def generate_coordinates(rows, cols):
-    x = np.arange(cols)
-    x_only = np.tile(x, (rows, 1))
-
-    y = np.arange(rows)
-    y_only = np.tile(y, (cols, 1))
-    grid = np.stack([x_only, np.transpose(y_only), np.zeros((rows, cols))], axis=-1)
-    grid = grid.reshape((-1, 3))
-    return grid[::-1, :]
-
-
-def add_additions(grid, additions):
-
-    return np.append(grid, np.transpose(additions),
-                     axis=0)
-
 def create_brain():
 
     sim.setup(timestep=0.1, min_delay=0.1, max_delay=20.0, threads=1, rng_seeds=[1234])
@@ -43,42 +27,18 @@ def create_brain():
 
     depressing_syn = sim.TsodyksMarkramSynapse(U=0.5, tau_rec=800.0, tau_facil=0.0,
                                                weight=0.01, delay=0.1)
-    facilitating_syn = sim.TsodyksMarkramSynapse(U=0.5, tau_rec=800.0,
-                                                 tau_facil=1000.0, weight=0.01,
+    facilitating_syn = sim.TsodyksMarkramSynapse(U=0.5, tau_rec=20.0,
+                                                 tau_facil=100.0, weight=0.1,
                                                  delay=0.1)
 
     cell_class = sim.IF_cond_exp(**SENSORPARAMS)
 
-    retina_neurons = 30, 40
-    total_retina = int(np.prod(retina_neurons))
-    neurons = sim.Population(11 + total_retina, cellclass=cell_class)
-
-    additions =  [[-1, -2, -3, -1, -2, -3, -1.5, -2.5, 10, 20, 30],
-                  [0, 0, 0, 1, 1, 1, 2, 2, 15, 15, 15],
-                  [0, 0, 0, 0, 0, 0, 0, 0, -5, -5, -5]]
-    positions = add_additions(generate_coordinates(*retina_neurons), additions)
-
-    neurons.positions = np.transpose(positions)
-    retina = neurons[0:total_retina]
-    xyt = neurons[total_retina:total_retina + 3]
-    sxt = neurons[total_retina + 3:total_retina + 6]
-    minxyt_1 = neurons[total_retina + 6: total_retina + 8]
-    greens = neurons[total_retina + 8: total_retina + 11]
-
-    """sim.Projection(presynaptic_population=retina,
-                   postsynaptic_population=retina,
-                   connector=sim.DistanceDependentProbabilityConnector("d<1.5"),  # Connect all neurons in the neighborhood
-                   space=sim.Space("xyz"),
-                   synapse_type=sim.StaticSynapse(weight=1/8, delay=0.1),
-                   receptor_type="excitatory")"""
+    neurons = sim.Population(6, cellclass=cell_class)
 
 
     sim.initialize(neurons, v=neurons.get('v_rest'))
-    return neurons, total_retina
+    return neurons
 
-circuit, total_retina = create_brain()
-retina = circuit[0:total_retina]
-xyt = circuit[total_retina:total_retina + 3]
-sxt = circuit[total_retina + 3:total_retina + 6]
-minxyt_1 = circuit[total_retina + 6:total_retina + 8]
-greens = circuit[total_retina + 8:total_retina + 11]
+circuit = create_brain()
+greens = circuit[:3]
+prediction = circuit[3:]
